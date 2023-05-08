8fad2b9aed53a27a3f5869375f724695406d7b7a
Varvara Babych <varvara.babich13@gmail.com>
2023-05-08 16:42:19 +0300

    add all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Searchbar } from './Searchbar/Searchbar';
+import { Component } from 'react';
+import css from './App.module.css';
+
+export class App extends Component {
+  state = {
+    materials: [],
+    searching: '',
+    page: 1,
+  };
+
+  incrementPage = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
+  handleFormSubmit = searching => {
+    this.setState({ searching, page: 1 });
+  };
+
+  render() {
+    return (
+      <section className={css.App}>
+        <div>
+          <Searchbar handleFormSubmit={this.handleFormSubmit} />
+          <ImageGallery
+            page={this.state.page}
+            searching={this.state.searching}
+            incrementPage={this.incrementPage}
+          />
+        </div>
+      </section>
+    );
+  }
+}
